1a8033e6522a12700ad41297a796bc8b1ae6ec20
Estêvão <108540477+ESTEV40@users.noreply.github.com>
2023-04-28 20:28:39 -0300

    Update soma.c

--- Estrutura de Dados/Prática.02/soma.c
+++ Estrutura de Dados/Prática.02/soma.c
@@ -1,21 +1,18 @@
-//Estêvão Sousa - 202210345
-//Larissa de Brito - 202210349
-//Davi Roriz - 202210342
 #include <stdio.h>
-
+//Soma Cruzada
 int main(){
     int N, i, j;
-    while (scanf("%d", &N)==1){
+    while (scanf("%d", &N)==1){//Acaba com o fim das entradas
 
         int Vetor_1[N], Vetor_2[N];
 
-        for(i=0; i < N; i++)//RECEBA 1
+        for(i=0; i < N; i++) //RECEBA 1
             scanf("%d", &Vetor_1[i]);
 
-        for(i=0; i < N; i++)//RECEBA 2
+        for(i=0; i < N; i++) //RECEBA 2
             scanf("%d", &Vetor_2[i]);
 
-        for( i=0 , j=(N-1) ; i<N ; i++ , j--)
+        for( i=0 , j=(N-1) ; i<N ; i++ , j--)// Soma o ultimo com o primeiro, o segundo com penultimo...
             printf("%d ", Vetor_1[i]+Vetor_2[j]);
             
         printf("\n");
